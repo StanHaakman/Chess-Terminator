abbd547f1b8abe41ddcbd93be7579facac904a70
Stan Haakman <stanhaakman@icloud.com>
2021-08-16 16:28:24 +0200

    Add framework for available moves in pawn

--- chessgame/pieces/pawn.py
+++ chessgame/pieces/pawn.py
@@ -5,3 +5,16 @@ class Pawn(Piece):
 
 	def __init__(self, color: bool):
 		super().__init__(color)
+
+	def available_moves(self):
+		"""
+
+		:return moves: list:
+		"""
+
+		moves = []
+
+		# TODO: create the rules for the pawn to move
+		# TODO: create the rules for the pawn to attack
+
+		return moves
