cbe4f7fb4d47ce5708ba481ebe8dad1a4c11a073
roosm <svroosmalen123@gmail.com>
2021-08-16 16:40:10 +0200

    add function ideas for the chessboard and chessgame classes

--- chessgame/chessBoard.py
+++ chessgame/chessBoard.py
@@ -3,3 +3,6 @@ class Board:
 
     def __init__(self):
         pass
+
+    def create_board(self):
+        # TODO: create a 8x8 matrix with zeros
--- chessgame/chessGame.py
+++ chessgame/chessGame.py
@@ -3,3 +3,7 @@ class Game:
 
     def __init__(self):
         pass
+
+    def set_piece(self, piece, location):
+        # TODO: Place the piece on the location in the matrix
+        return -1
